6dbfec398b4789ed735363e4d1b168bff767caed
abcdevelop <bruno.panaget@yahoo.fr>
2017-10-17 14:41:04 +0200

    Add find collections

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,13 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+
+// var user = {name:'Bruno',age:50};
+// var {name}=user;
+// console.log(name);
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
 
 MongoClient.connect('Mongodb://localhost:27017/TodoApp',(err, db)=>{
     if(err){
@@ -18,17 +27,18 @@ MongoClient.connect('Mongodb://localhost:27017/TodoApp',(err, db)=>{
     //     console.log(JSON.stringify(result.ops,undefined,2));
     // });
 
-    // db.collection('Users').insertOne({
-    //     name:'Bruno',
-    //     age:50,
-    //     location:'Paris'
-    // },(err,result)=>{
-    //     if(err){
-    //         return console.log('Unable to insert user',err);
-    //     }
-    //
-    //     console.log(JSON.stringify(result.ops,undefined,2));
-    // });
+    db.collection('Users').insertOne({
+        name:'Bruno',
+        age:50,
+        location:'Paris'
+    },(err,result)=>{
+        if(err){
+            return console.log('Unable to insert user',err);
+        }
+
+        console.log(JSON.stringify(result.ops,undefined,2));
+        //console.log(result.ops[0]._id.getTimestamp();
+    });
 
 
     db.close();
