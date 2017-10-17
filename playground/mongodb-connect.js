85a644408e8e5c2b68fe7cdd751f46667e6acb36
DELL-BRUNO\Bruno <bruno.panaget@yahoo.fr>
2017-10-17 19:45:38 +0200

    Synchro

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -27,18 +27,18 @@ MongoClient.connect('Mongodb://localhost:27017/TodoApp',(err, db)=>{
     //     console.log(JSON.stringify(result.ops,undefined,2));
     // });
 
-    db.collection('Users').insertOne({
-        name:'Bruno',
-        age:50,
-        location:'Paris'
-    },(err,result)=>{
-        if(err){
-            return console.log('Unable to insert user',err);
-        }
-
-        console.log(JSON.stringify(result.ops,undefined,2));
-        //console.log(result.ops[0]._id.getTimestamp();
-    });
+    // db.collection('Users').insertOne({
+    //     name:'Bruno',
+    //     age:50,
+    //     location:'Paris'
+    // },(err,result)=>{
+    //     if(err){
+    //         return console.log('Unable to insert user',err);
+    //     }
+    //
+    //     console.log(JSON.stringify(result.ops,undefined,2));
+    //     //console.log(result.ops[0]._id.getTimestamp();
+    // });
 
 
     db.close();
